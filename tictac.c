0845b680df1271e1c7b95a07dd948cf8406585b5
JFinn05 <g00464466@atu.ie>
2025-04-08 17:01:32 +0100

    Update tictac.c
    finished my part

--- tictac.c
+++ tictac.c
@@ -93,7 +93,22 @@ void playerMove() {
 }
 
 void computerMove() { 
-   
+   srand(time(0));
+   int x;
+   int y;
+
+   if(checkFreeSpaces()>0){
+    do{
+        x = rand() % 3;
+        y = rand() % 3;
+
+    }while(board[x][y] !=' ');
+
+    board[x][y] = COMPUTER;
+   }
+   else{
+    printWinner(' ');
+   }
 }
 
 char checkWinner() { 
@@ -108,5 +123,13 @@ char checkWinner() {
 }
 
 void printWinner(char winner) { 
-    
+    if(winner == PLAYER){
+        printf("YOU WIN!!");
+    }
+    else if (winner == COMPUTER){
+        printf("YOU LOSE!!");
+    }
+    else{
+        printf("ITS A DRAW!!");
+    }
 }
